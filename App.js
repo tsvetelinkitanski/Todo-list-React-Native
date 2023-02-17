bb413bdd86faccd3a644269ed259371bd534b91f
Tsvetelin <tskitanski@Tsvetelins-MacBook-Air.local>
2023-02-17 13:18:16 +0200

    First commit

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>Today's tasks</Text>
+        <View style={styles.items}></View>
+      </View>
     </View>
   );
 }
@@ -13,8 +15,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#E8EAED",
   },
+  wrapper: {},
+  title: {
+    fontSize: "24",
+    fontWeight: "700",
+    position: "absolute",
+    color: "#1A1A1A",
+    top: 94,
+    left: 20,
+  },
+  items: {},
 });

3d4b3e4bcd21a0b8c8bde677e998174e4e66662a
tsvetelinkitanski <kitanskitsvetelin@gmail.com>
2023-02-17 21:10:55 +0200

    main style without functionality

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Task from "./components/Task";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.wrapper}>
         <Text style={styles.title}>Today's tasks</Text>
-        <View style={styles.items}></View>
+        <View style={styles.items}>
+          <Task text={"Text 1"} />
+          <Task text={"Text 2"} />
+        </View>
       </View>
     </View>
   );
@@ -17,14 +21,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#E8EAED",
   },
-  wrapper: {},
+  wrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
   title: {
     fontSize: "24",
-    fontWeight: "700",
-    position: "absolute",
-    color: "#1A1A1A",
-    top: 94,
-    left: 20,
+    fontWeight: "bold",
   },
-  items: {},
+  items: {
+    marginTop: 30,
+  },
 });
